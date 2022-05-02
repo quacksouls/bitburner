4416b8bf1e1f62ad2bdc2f3e7c04244dced023a2
quacksouls <66396308+quacksouls@users.noreply.github.com>
2022-05-02 13:51:16 +1000

    Robust handling of number of threads.
    Calculate how many threads we can run our hack script on a purchased server.

--- 04/buy-server.js
+++ 04/buy-server.js
@@ -1,13 +1,13 @@
 /**
- * Whether we have the necessary programs on our home server to allow us to gain
- * root access on other servers.
+ * Whether we have the necessary programs on our home server to allow us to 
+ * hack other servers.
  * 
  * @param ns The Netscript API.
  * @return true if we have the necessary programs; false otherwise.
  */
 function have_programs(ns) {
 	const home = "home";
-	const program = ["BruteSSH.exe", "FTPCrack.exe", "NUKE.exe"];
+	const program = ["BruteSSH.exe", "FTPCrack.exe", "hack.js", "NUKE.exe"];
 
 	// Ensure we have the prerequisite programs on our home server.
 	for (const p of program) {
@@ -25,9 +25,8 @@ function have_programs(ns) {
  * @param {NS} ns
  */
 export async function main(ns) {
-	// We want each server to have 16GB of RAM.  We will use 6 threads to run
-	// the hack.js script on a server.
-	const ram = 16;
+	// We want each server to have 16GB of RAM.
+	const server_ram = 16;
 	// The ID of a purchased server.
 	var i = 0;
 	// Our hack script.
@@ -35,7 +34,8 @@ export async function main(ns) {
 	// Our hack script is located on our home server.
 	const source = "home";
 	// How many threads to run our script on a purchased server.
-	const nthread = 6;
+	const script_ram = ns.getScriptRam(script, source);
+	const nthread = Math.floor(server_ram / script_ram);
 	// Hack this target.
 	const target = "neo-net";
 	// 10,000 milliseconds or 10 seconds.
@@ -59,13 +59,13 @@ export async function main(ns) {
 	// number of servers we can buy.
 	while (i < ns.getPurchasedServerLimit()) {
 		// Do we have enough money (on our home server) to buy a new server?
-		if (ns.getServerMoneyAvailable(source) > ns.getPurchasedServerCost(ram)) {
+		if (ns.getServerMoneyAvailable(source) > ns.getPurchasedServerCost(server_ram)) {
 			// If we have enough money, then:
-			// (1) purchase a new server;
-			const hostname = ns.purchaseServer("pserv-" + i, ram);
-			// (2) copy our hack script over to the purchased server;
+			// purchase a new server;
+			const hostname = ns.purchaseServer("pserv-" + i, server_ram);
+			// copy our hack script over to the purchased server;
 			await ns.scp(script, source, hostname);
-			// (3) run our hack script on the purchased server.
+			// run our hack script on the purchased server.
 			ns.exec(script, hostname, nthread, target);
 			i++;
 		}
