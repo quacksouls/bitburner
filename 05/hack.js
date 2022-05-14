4ccd6bc8b91b95f908df29bd450f5b0927cf45ab
quacksouls <66396308+quacksouls@users.noreply.github.com>
2022-05-14 10:34:27 +1000

    Improve readability of script.

--- 05/hack.js
+++ 05/hack.js
@@ -1,62 +1,52 @@
+import { assert, Server } from "./libbnr.js";
+
 /**
  * Hack a server and steal its money.  We weaken the server's security
  * as necessary, grow the server in case the amount of money on the server
  * is below our threshold, and hack the server when all conditions are met.
- * Code adapted from the tutorial at
- * https://bitburner.readthedocs.io/en/latest/guidesandtips/gettingstartedguideforbeginnerprogrammers.html
+ * 
+ * Usage: run hack.js [targetServer]
  *
  * @param ns Command line arguments given to the script at run time.  We only
  *     want one command line argument, i.e. the name of the server we want to hack.
  */
 export async function main(ns) {
 	// The target server, i.e. the server to hack.
-	const target = ns.args[0];
+	const target = new Server(ns, ns.args[0]);
 
 	// Ensure we have root access on the target server.
-	if (!ns.hasRootAccess(target)) {
+	if (!target.has_root_access()) {
 		try {
-			await ns.brutessh(target);
-		} catch {}
-		try {
-			await ns.ftpcrack(target);
-		} catch {}
-		try {
-			await ns.httpworm(target);
-		} catch {}
-		try {
-			await ns.relaysmtp(target);
-		} catch {}
-		try {
-			await ns.sqlinject(target);
-		} catch {}
-		try {
-			await ns.nuke(target);
+			await target.gain_root_access();
 		} catch {
-			ns.alert("Can't gain root access to " + target);
+			await ns.tprint("Can't gain root access to server: " + target.hostname());
 			ns.exit();
 		}
 	}
 
-	// How much money a server should have before we hack it.
-	// Set the money threshold at 75% of the server's maximum money.
-	const moneyThresh = ns.getServerMaxMoney(target) * 0.75;
+	// How much money a server should have before we hack it.  Assume the target is
+	// not bankrupt.  Set the money threshold at 75% of the server's maximum money.
+	assert(!target.is_bankrupt());
+	const money_threshold = target.money_max() * 0.75;
+	assert(money_threshold > 0);
 
 	// The threshold for the server's security level.  If the target's
 	// security level is higher than the threshold, weaken the target
 	// before doing anything else.
-	const securityThresh = ns.getServerMinSecurityLevel(target) + 5;
+	const security_threshold = target.security_min() + 5;
 
-	// Infinite loop that continously hacks/grows/weakens the target server.
+	// Continuously hack/grow/weaken the target server.
 	while (true) {
-		if (ns.getServerSecurityLevel(target) > securityThresh) {
+		const money_available = target.money_available();
+		if (target.security_level() > security_threshold) {
 			// If the server's security level is above our threshold, weaken it.
-			await ns.weaken(target);
-		} else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
+			await target.weaken();
+		} else if (money_available < money_threshold) {
 			// If the server's money is less than our threshold, grow it.
-			await ns.grow(target);
+			await target.grow();
 		} else {
 			// Otherwise, hack it.
-			await ns.hack(target);
+			await target.hack();
 		}
 	}
 }
