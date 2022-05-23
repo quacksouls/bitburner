8f01f1198aec3e41a811a2900826f3389d184b03
quacksouls <66396308+quacksouls@users.noreply.github.com>
2022-05-23 15:04:28 +1000

    Continuously try to hack low-end servers.

--- 05/low-end.js
+++ 05/low-end.js
@@ -1,22 +1,50 @@
-import { assert, filter_bankrupt_servers, filter_pserv, network, Player, Server } from "./libbnr.js";
+import { assert, filter_pserv, minutes_to_milliseconds, network, Player, Server } from "./libbnr.js";
+
+/**
+ * Whether there are new low-end servers we can now hack.
+ *
+ * @param current_ln An array of low-end servers that we are now hacking.
+ * @param new_ln An array of low-end servers, possibly including new
+ *     low-end servers that we can now hack.
+ * @return true if we can now hack more low-end servers; false otherwise.
+ */
+function has_new_low_end(current_ln, new_ln) {
+	assert(current_ln.length >= 0);
+	assert(new_ln.length > 0);
+	assert(current_ln.length <= new_ln.length);
+	const NEW = true;      // Have new low-end servers to hack.
+	const NO_NEW = !NEW;  // No new low-end servers to hack.
+
+	if (current_ln.length < new_ln.length) {
+		return NEW;
+	}
+
+	// If the two arrays are of equal length, ensure that both
+	// have the same low-end servers.
+	assert(current_ln.length == new_ln.length);
+	const new_set = new Set(new_ln);
+	for (const s of current_ln) {
+		assert(new_set.has(s));
+	}
+	return NO_NEW;
+}
 
 /**
  * Choose servers in the game world that are low-end.  A server is low-end if
  * it does not have enough RAM to run our hack script even using one thread.
- * We exclude these servers:
- * 
- *     * Purchased servers.
- *     * Those servers in the game world that are bankrupt.
- *     * A world server whose hacking skill requirement is higher than
- *       our Hack stat.
+ *
+ * A bankrupt server can be low-end if it lacks the required amount of RAM to
+ * run our hack script using one thread.  Although we would not obtain any money
+ * from hacking a low-end bankrupt server, we would still obtain some hacking
+ * points.
  *
  * @param ns The Netscript API.
  * @return An array of low-end servers.
  */
 function low_end_servers(ns) {
-	let server = filter_bankrupt_servers(ns, filter_pserv(ns, network(ns)));
-	let lowend = new Array();
+	const server = filter_pserv(ns, network(ns));
 	const player = new Player(ns);
+	let lowend = new Array();
 	for (const s of server) {
 		if (skip_server(ns, s)) {
 			continue;
@@ -32,7 +60,14 @@ function low_end_servers(ns) {
 
 /**
  * Whether to skip a server.  A server is skipped if it is not a low-end server.
+ * We exclude these servers:
  * 
+ *     * Purchased servers.
+ *     * A world server whose hacking skill requirement is higher than
+ *       our Hack stat.
+ *     * A world server for which we cannot open all ports.
+ *     * A world server that is currently running our hacking script.
+ *
  * @param ns The Netscript API.
  * @param server Do we skip this server?
  * @return true if the given server should be skipped; false otherwise.
@@ -60,26 +95,52 @@ function skip_server(ns, server) {
 }
 
 /**
+ * Use our home server to hack any new low-end servers that we can now compromise.
+ * 
+ * @param ns The Netscript API.
+ * @param lowend An array of low-end servers.
+ * @return An array of low-end servers, possibly updated to include new low-end
+ *     servers that have been hacked during this update.
+ */
+async function update(ns, lowend) {
+	assert(lowend.length >= 0);
+	const player = new Player(ns);
+	const target = low_end_servers(ns);
+
+	// Hack the new low-end servers.
+	if (has_new_low_end(lowend, target)) {
+		// First, kill all instances of the hack script.
+		for (const server of lowend) {
+			ns.kill(player.script(), player.home(), server);
+		}
+		// Next, hack all low-end servers we can now visit.
+		const home = new Server(ns, player.home());
+		const nthread = home.threads_per_instance(player.script(), target.length);
+		for (const server of target) {
+			ns.exec(player.script(), player.home(), nthread, server);
+		}
+	}
+	return target;
+}
+
+/**
  * Hack various low-end servers found in the game world, excluding purchased servers.
  * A world server is said to be low-end if it does not have enough RAM to run our
  * hack script on the server.  We use our home server to hack low-end servers.
- * The script accepts the number of threads to use to hack a low-end server.
+ * The script figures out how many threads to use to hack a low-end server.
  * 
- * Usage: run low-end.js [numThread]
- * Example: run low-end.js 4
+ * Usage: run low-end.js
  *
  * @param ns The Netscript API.
  */
 export async function main(ns) {
-	const nthread = Math.floor(ns.args[0]);
-	assert(nthread > 0);
+	const time = minutes_to_milliseconds(10);
+	let target = new Array();
 
-	const target = low_end_servers(ns);
-	if (target.length < 1) {
-		ns.exit();
-	}
-	const player = new Player(ns);
-	for (const server of target) {
-		ns.exec(player.script(), player.home(), nthread, server);
+	// Continuously try to search for low-end servers to hack.
+	while (true) {
+		target = await update(ns, target);
+		await ns.print("Low-end servers: " + target.join(" "));
+		await ns.sleep(time);
 	}
 }
