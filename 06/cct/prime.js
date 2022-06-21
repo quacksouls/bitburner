057e23eb5800b6a74d93298fbf94abbb53e773b7
Duck McSouls <66396308+quacksouls@users.noreply.github.com>
2022-06-21 19:38:32 +1000

    Log failure when solving a coding contract.

--- 06/cct/prime.js
+++ 06/cct/prime.js
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { assert } from "./libbnr.js";
+import { assert, log_cct_failure } from "./libbnr.js";
 
 /**
  * Obtain a factor of a positive integer.
@@ -145,5 +145,12 @@ export async function main(ns) {
     const result = ns.codingcontract.attempt(
         max_prime_factor(n), cct, host, { returnReward: true }
     );
+    // Log the result in case of failure.
+    if (0 == result.length) {
+        const log = "/cct/prime.txt";
+        await log_cct_failure(ns, log, cct, host, n);
+        ns.tprint(host + ": " + cct + ": FAILURE");
+        return;
+    }
     ns.tprint(host + ": " + cct + ": " + result);
 }
