37a57368b0bf25d501bbca0c0b70a8d90f3b6713
Duck McSouls <66396308+quacksouls@users.noreply.github.com>
2022-06-29 12:42:12 +1000

    Refactor a function.

--- 06/cct/trader3.js
+++ 06/cct/trader3.js
@@ -15,56 +15,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { assert, log_cct_failure, max_profit_kadane } from "./libbnr.js";
-
-/**
- * The maximimum profit that can be made when we are restricted to at most
- * two transactions.  Transactions must occur one after the other.  Suppose
- * we buy shares of a stock on day i and sell the shares on day j, where
- * i < j.  We cannot buy shares of the same stock on any day between i and j.
- * However, we are allowed to buy more shares of the stock from day j+1 onward.
- *
- * The above description hints at a simple solution.  We partition the price
- * array into two, non-overlapping parts:
- *
- * (1) The left part starts from day 0 and ends at day k, inclusive.  Run
- *     Kadane's algorithm on the left subarray to get mpl as our maximum profit
- *     for the left subarray.
- * (2) The right part starts from day k+1 and ends at the last day in the price
- *     array.  Run Kadane's algorithm on the right subarray to get mpr as our
- *     maximum profit for the right subarray.
- *
- * The maximum profit is mpl + mpr.  This maximum profit is for one particular
- * partition of the price array.  There are many other partitions, one for each
- * value of k.  Calculate the maximum profit for each partition.  The true
- * maximum profit is the maximum of the results of all partitions.
- *
- * @param price An array of prices, where price[i] is the price of a stock
- *     on day i.  All prices relate to the same stock.
- * @return The maximum profit to be made, assumming we can perform at most
- *     two transactions.  Return 0 if no profit can be made.
- */
-function maximize_profit(price) {
-    assert(price.length > 0);
-    // Obtain all possible partitions of the price array.  Each partition
-    // divides the array into two parts: the left subarray and the right
-    // subarray.
-    let max_profit = 0;
-    for (let k = 0; k < price.length; k++) {
-        // The left and right subarrays in the partition.
-        const left = price.slice(0, k + 1);
-        const right = price.slice(k + 1, price.length);
-        // The maximum profit of each subarray in the partition.
-        const mpl = max_profit_kadane(left);
-        let mpr = 0;
-        if (right.length > 0) {
-            mpr = max_profit_kadane(right);
-        }
-        // The running maximum profit.
-        max_profit = Math.max(max_profit, mpl + mpr);
-    }
-    return max_profit;
-}
+import { log_cct_failure, stock_traderIII } from "./libbnr.js";
 
 /**
  * Algorithmic Stock Trader III: You are given an array of numbers representing
@@ -91,7 +42,7 @@ export async function main(ns) {
     // Solve the coding contract.
     const array = ns.codingcontract.getData(cct, host);
     const result = ns.codingcontract.attempt(
-        maximize_profit(array), cct, host, { returnReward: true }
+        stock_traderIII(array), cct, host, { returnReward: true }
     );
     // Log the result in case of failure.
     if (0 == result.length) {
--- 06/libbnr.js
+++ 06/libbnr.js
@@ -1365,3 +1365,53 @@ export function shortest_path(ns, source, target) {
     // A shortest path from source to target.
     return graph.shortest_path(source, target);
 }
+
+/**
+ * The maximimum profit that can be made when we are restricted to at most two
+ * transactions.  Transactions must occur one after the other.  Suppose we buy
+ * one share of a stock on day i and sell the share on day j, where i < j.  We
+ * cannot buy another share of the same stock on any day between i and j.
+ * However, we are allowed to buy another share of the stock from day j+1
+ * onward.
+ *
+ * The above description hints at a simple solution.  We partition the price
+ * array into two non-overlapping parts:
+ *
+ * (1) The left part starts from day 0 and ends at day k, inclusive.  Run
+ *     Kadane's algorithm on the left subarray to get mpl as our maximum profit
+ *     for the left subarray.
+ * (2) The right part starts from day k+1 and ends at the last day in the price
+ *     array.  Run Kadane's algorithm on the right subarray to get mpr as our
+ *     maximum profit for the right subarray.
+ *
+ * The maximum profit is mpl + mpr.  This maximum profit is for one particular
+ * partition of the price array.  There are many other partitions, one for each
+ * value of k.  Calculate the maximum profit for each partition.  The true
+ * maximum profit is the maximum of the results of all partitions.
+ *
+ * @param price An array of prices, where price[i] is the price of one share of
+ *     a stock on day i.  All prices relate to the same stock.
+ * @return The maximum profit to be made, assumming we can perform at most
+ *     two transactions.  Return 0 if no profit can be made.
+ */
+export function stock_traderIII(price) {
+    assert(price.length > 0);
+    // Obtain all possible partitions of the price array.  Each partition
+    // divides the array into two parts: the left subarray and the right
+    // subarray.
+    let max_profit = 0;
+    for (let k = 0; k < price.length; k++) {
+        // The left and right subarrays in the partition.
+        const left = price.slice(0, k + 1);
+        const right = price.slice(k + 1, price.length);
+        // The maximum profit of each subarray in the partition.
+        const mpl = max_profit_kadane(left);
+        let mpr = 0;
+        if (right.length > 0) {
+            mpr = max_profit_kadane(right);
+        }
+        // The running maximum profit.
+        max_profit = Math.max(max_profit, mpl + mpr);
+    }
+    return max_profit;
+}
