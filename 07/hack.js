d3fd6d4fb7ffd3f9e8710a7d6982c37c098c1bb9
Duck McSouls <66396308+quacksouls@users.noreply.github.com>
2022-07-04 13:17:52 +1000

    Reduce the RAM cost of our hack script.

--- 07/hack.js
+++ 07/hack.js
@@ -15,15 +15,46 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { Server } from "/libbnr.js";
+// Keep this script as independent as possible so that its RAM requirement is
+// as low as possible.  If we import anything, ensure that the imported class
+// or function has minimal RAM requirement.
+
+import { Time } from "/lib.time.js";
 
 /**
- * Hack a server and steal its money.  We weaken the server's security
- * as necessary, grow the server in case the amount of money on the server
- * is below our threshold, and hack the server when all conditions are met.
- * This script accepts the first command line argument given to the script
- * at runtime.  We want one command line argument, i.e. the name of the
- * server we want to hack.
+ * Try to gain root access on a server.
+ *
+ * @param ns The Netscript API.
+ * @param server We want root access on this server.
+ * @return true if we have root access on this server;
+ *     false if root access cannot be obtained.
+ */
+async function gain_root_access(ns, server) {
+    // Do we already have root access on this server?
+    if (ns.hasRootAccess(server)) {
+        return true;
+    }
+    // Try to open all required ports and nuke the server.
+    try { await ns.brutessh(server); } catch { }
+    try { await ns.ftpcrack(server); } catch { }
+    try { await ns.httpworm(server); } catch { }
+    try { await ns.relaysmtp(server); } catch { }
+    try { await ns.sqlinject(server); } catch { }
+    try {
+        await ns.nuke(server);
+        return true;
+    } catch {
+        return false;
+    }
+}
+
+/**
+ * Hack a server and steal its money.  We weaken the server's security as
+ * necessary, grow the server in case the amount of money on the server is
+ * below our threshold, and hack the server when all conditions are met.  This
+ * script accepts the first command line argument given to the script at
+ * runtime.  We want one command line argument, i.e. the name of the* server we
+ * want to hack.
  *
  * Usage: run hack.js [targetServer]
  * Example: run hack.js n00dles
@@ -32,13 +63,13 @@ import { Server } from "/libbnr.js";
  */
 export async function main(ns) {
     // The target server, i.e. the server to hack.
-    const target = new Server(ns, ns.args[0]);
-    // Ensure we have root access to the target server.
-    if (!target.has_root_access()) {
+    const target = ns.args[0];
+    // Ensure we have root access on the target server.
+    if (!ns.hasRootAccess(target)) {
         try {
-            await target.gain_root_access();
+            await gain_root_access(ns, target);
         } catch {
-            const error_msg = "Can't gain root access to " + target.hostname();
+            const error_msg = "Can't gain root access to " + target;
             await ns.tprint(error_msg);
             ns.exit();
         }
@@ -47,23 +78,26 @@ export async function main(ns) {
     // server is bankrupt, successfully hacking it would increase our hacking
     // experience points, although we would not receive any money.
     // Set the money threshold at 75% of the server's maximum money.
-    const money_threshold = target.money_max() * 0.75;
+    const money_threshold = ns.getServerMaxMoney(target) * 0.75;
     // The threshold for the server's security level.  If the target's
     // security level is higher than the threshold, weaken the target
     // before doing anything else.
-    const security_threshold = target.security_min() + 5;
+    const security_threshold = ns.getServerMinSecurityLevel(target) + 5;
     // Continuously hack/grow/weaken the target server.
+    const time = new Time();
+    const t = time.millisecond();
     while (true) {
-        const money_available = target.money_available();
-        if (target.security_level() > security_threshold) {
+        const money_available = ns.getServerMoneyAvailable(target);
+        if (ns.getServerSecurityLevel(target) > security_threshold) {
             // If the server's security level is above our threshold, weaken it.
-            await target.weaken();
+            await ns.weaken(target);
         } else if (money_available < money_threshold) {
             // If the server's money is less than our threshold, grow it.
-            await target.grow();
+            await ns.grow(target);
         } else {
             // Otherwise, hack it.
-            await target.hack();
+            await ns.hack(target);
         }
+        await ns.sleep(t);
     }
 }
