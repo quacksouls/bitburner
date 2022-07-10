ebd0ceeb1e8add94e1d83827f0cfeff929a30ca0
Duck McSouls <66396308+quacksouls@users.noreply.github.com>
2022-07-10 15:07:58 +1000

    Reserve more RAM on home server.

--- 06/lib/server.js
+++ 06/lib/server.js
@@ -99,7 +99,9 @@ export class Server {
             this.#ram_reserve = default_ram;
             // Reserve a higher amount of RAM, depending on the maximum RAM on
             // the home server.
-            if (this.ram_max() >= 2048) {
+            if (this.ram_max() >= 4096) {
+                this.#ram_reserve = 2048;
+            } else if (this.ram_max() >= 2048) {
                 this.#ram_reserve = 1024;
             } else if (this.ram_max() >= 1024) {
                 this.#ram_reserve = 500;
