6bec81dea4919a3c68f8eeb22344f80787e77cc3
Duck McSouls <66396308+quacksouls@users.noreply.github.com>
2022-07-12 11:00:32 +1000

    Remove unnecessary await.

--- 06/shortest-path.js
+++ 06/shortest-path.js
@@ -30,13 +30,13 @@ export async function main(ns) {
     // Must provide a command line argument.
     const error_msg = "Must provide the name of the target server.";
     if (ns.args.length < 1) {
-        await ns.tprint(error_msg);
+        ns.tprint(error_msg);
         ns.exit();
     }
     const target = ns.args[0];
     const path = shortest_path(ns, "home", target);
     if (path.length < 1) {
-        await ns.tprint("Target server must be reachable from home.");
+        ns.tprint("Target server must be reachable from home.");
         ns.exit();
     }
     ns.tprint(path.join(" -> "));
