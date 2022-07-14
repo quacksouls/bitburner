4fb953386d100d39a69108655bb0580ca2cb8177
Duck McSouls <66396308+quacksouls@users.noreply.github.com>
2022-07-14 11:09:39 +1000

    Proper formatting.

--- 03/hack.js
+++ 03/hack.js
@@ -9,37 +9,33 @@
  *     want one command line argument, i.e. the name of the server we want to hack.
  */
 export async function main(ns) {
-	// The target server, i.e. the server to hack.
-	const target = ns.args[0];
-
-	// Ensure we have root access on the target server.  We assume that currently
-	// we have access to servers that require up to 2 ports to be open.
-	if (!ns.hasRootAccess(target)) {
-		await ns.brutessh(target);
-		await ns.ftpcrack(target);
-		await ns.nuke(target);
-	}
-
-	// How much money a server should have before we hack it.
-	// Set the money threshold at 75% of the server's maximum money.
-	const moneyThresh = ns.getServerMaxMoney(target) * 0.75;
-
-	// The threshold for the server's security level.  If the target's
-	// security level is higher than the threshold, weaken the target
-	// before doing anything else.
-	const securityThresh = ns.getServerMinSecurityLevel(target) + 5;
-
-	// Infinite loop that continously hacks/grows/weakens the target server.
-	while (true) {
-		if (ns.getServerSecurityLevel(target) > securityThresh) {
-			// If the server's security level is above our threshold, weaken it.
-			await ns.weaken(target);
-		} else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
-			// If the server's money is less than our threshold, grow it.
-			await ns.grow(target);
-		} else {
-			// Otherwise, hack it.
-			await ns.hack(target);
-		}
-	}
+    // The target server, i.e. the server to hack.
+    const target = ns.args[0];
+    // Ensure we have root access on the target server.  We assume that currently
+    // we have access to servers that require up to 2 ports to be open.
+    if (!ns.hasRootAccess(target)) {
+        await ns.brutessh(target);
+        await ns.ftpcrack(target);
+        await ns.nuke(target);
+    }
+    // How much money a server should have before we hack it.
+    // Set the money threshold at 75% of the server's maximum money.
+    const moneyThresh = ns.getServerMaxMoney(target) * 0.75;
+    // The threshold for the server's security level.  If the target's
+    // security level is higher than the threshold, weaken the target
+    // before doing anything else.
+    const securityThresh = ns.getServerMinSecurityLevel(target) + 5;
+    // Infinite loop that continously hacks/grows/weakens the target server.
+    while (true) {
+        if (ns.getServerSecurityLevel(target) > securityThresh) {
+            // If the server's security level is above our threshold, weaken it.
+            await ns.weaken(target);
+        } else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
+            // If the server's money is less than our threshold, grow it.
+            await ns.grow(target);
+        } else {
+            // Otherwise, hack it.
+            await ns.hack(target);
+        }
+    }
 }
