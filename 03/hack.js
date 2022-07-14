4902258af15abd8cfd5c4dc297b41119a683bef8
Duck McSouls <66396308+quacksouls@users.noreply.github.com>
2022-07-14 11:14:19 +1000

    Copyright header.

--- 03/hack.js
+++ 03/hack.js
@@ -1,4 +1,21 @@
 /**
+ * Copyright (C) 2022 Duck McSouls
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
  * Hack a server and steal its money.  We weaken the server's security
  * as necessary, grow the server in case the amount of money on the server
  * is below our threshold, and hack the server when all conditions are met.  We
