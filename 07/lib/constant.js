4250ffe7077bcf466dbfdd2c810cc13280b8d6f1
Duck McSouls <66396308+quacksouls@users.noreply.github.com>
2022-07-14 14:47:52 +1000

    Move a function to library.

--- 07/lib/constant.js
+++ 07/lib/constant.js
@@ -31,6 +31,59 @@ export function all_crimes() {
 }
 
 /**
+ * An array of all faction names.  The faction names are listed in a specific
+ * order, usually along the following line of reasoning.
+ *
+ * (1) Sector-12 should be the first faction we must join.  The reason is
+ *     simple.  This faction has the CashRoot Starter Kit Augmentation that
+ *     grants us $1m and the BruteSSH.exe program after a soft reset.
+ * (2) Netburners, CyberSec, and Tian Di Hui should be the next group of
+ *     factions to join.  These factions have Augmentations that raise various
+ *     hack-related stats.
+ * (3) BitRunners has an Augmentation that allows us to start with the
+ *     FTPCrack.exe and relaySMTP.exe programs after a soft reset.
+ * (4) Aevum has the PCMatrix Augmentation that allows us to start with
+ *     DeepscanV1.exe and AutoLink.exe after a soft reset.
+ * (5) Chongqing, Ishima, and New Tokyo are not enemies with each other.  We
+ *     can join all three factions at the same time, work toward purchasing all
+ *     of their Augmentations, and install all Augmentations from all three
+ *     factions at the same time.  Doing so can save us a lot of time as we do
+ *     not need to go through a soft reset after purchasing all Augmentations
+ *     from one faction.
+ * (6) Volhaven should be the last city faction to join.
+ * (7) NiteSec and The Black Hand are the remaining two hacking groups to join.
+ *     They have Augmentations to boost various hack-related stats.
+ * (8) We can join the megacorporation factions in any order we want.  These
+ *     factions have various Augmentations that boost a number of
+ *     social-related stats, i.e. reputation from factions and companies.
+ * (9) Criminal organizations have Augmentations to boost various combat stats
+ *     as well as social-related stats.
+ * (10) The endgame factions should be the last to join.  We can join
+ *     Illuminati or The Covenant in any order we want.  However, Daedalus
+ *     should be the very last faction that we join.
+ */
+export function all_factions() {
+    const faction = [
+        // Early game factions, city factions, and hacking groups.
+        "Sector-12", "Netburners", "CyberSec", "Tian Di Hui", "BitRunners",
+        "Aevum", "Chongqing", "Ishima", "New Tokyo", "Volhaven", "NiteSec",
+        "The Black Hand",
+        // Megacorporations.
+        "Bachman & Associates", "Blade Industries", "Clarke Incorporated",
+        "ECorp", "Four Sigma", "Fulcrum Secret Technologies",
+        "KuaiGong International", "MegaCorp", "NWO", "OmniTek Incorporated",
+        // Criminal organizations.  The Syndicate has the Augmentation
+        // BrachiBlades, which is a pre-requisite of an Augmentation from
+        // Speakers for the Dead.
+        "Silhouette", "Slum Snakes", "The Syndicate", "Speakers for the Dead",
+        "Tetrads", "The Dark Army",
+        // Endgame factions.
+        "Illuminati", "The Covenant", "Daedalus"
+    ];
+    return faction;
+}
+
+/**
  * All programs that can be created.  These programs can also be purchased via
  * the dark web.
  */
