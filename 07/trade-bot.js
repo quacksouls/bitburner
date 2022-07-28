06a23f4ad9c373453f2f484045abdf3d22343399
Duck McSouls <66396308+quacksouls@users.noreply.github.com>
2022-07-28 21:40:03 +1000

    Skip buying shares of stocks.

--- 07/trade-bot.js
+++ 07/trade-bot.js
@@ -273,7 +273,17 @@ function sell_stock(ns, stk) {
  *     false otherwise.
  */
 function skip_buy(ns) {
-    return ns.fileExists(trade_bot_stop, home);
+    const SKIP = true;
+    const NO_SKIP = !SKIP;
+    if (ns.fileExists(trade_bot_stop, home)) {
+        const m = new Money();
+        const threshold = m.trillion();
+        const player = new Player(ns);
+        if (player.money() < threshold) {
+            return SKIP;
+        }
+    }
+    return NO_SKIP;
 }
 
 /**
