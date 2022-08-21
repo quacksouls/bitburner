908a9a02cb39d90d3c4fd5412a42d65ecac4b0f5
Duck McSouls <66396308+quacksouls@users.noreply.github.com>
2022-08-21 11:43:00 +1000

    Print the gain in Intelligence XP.

--- 08/intelligence/util.js
+++ 08/intelligence/util.js
@@ -15,6 +15,8 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+import { assert } from "/lib/util.js";
+
 // Miscellaneous utility functions.
 
 /**
@@ -24,4 +26,26 @@
  */
 export function intelligence(ns) {
     return ns.getPlayer().exp.intelligence;
+}
+
+/**
+ * Print the gain in Intelligence XP.
+ *
+ * @param ns The Netscript API.
+ * @param before The amount of Intelligence XP before a certain action.
+ * @param after The amount of Intelligence XP after performing a certain
+ *     action.
+ * @param action The action whose performance might possibly result in a gain
+ *     in Intelligence XP.
+ */
+export function intelligence_gain(ns, before, after, action) {
+    assert(before >= 0);
+    assert(after >= 0);
+    assert(action.length > 0);
+    const gain = after - before;
+    ns.tprint(action);
+    ns.tprint("Intelligence before: " + before);
+    ns.tprint("Intelligence after: " + after);
+    ns.tprint("Intelligence gain: " + gain);
+    ns.tprint("");
 }
