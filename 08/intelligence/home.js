070481a5bc28a8d3234ed67172a7d300ad923885
Duck McSouls <66396308+quacksouls@users.noreply.github.com>
2022-08-21 10:47:25 +1000

    Intelligence XP from upgrading RAM and Cores on home server.

---
+++ 08/intelligence/home.js
@@ -0,0 +1,79 @@
+/**
+ * Copyright (C) 2022 Duck McSouls
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { intelligence } from "/intelligence/util.js";
+import { home } from "/lib/constant.js";
+import { Player } from "/lib/player.js";
+import { Server} from "/lib/server.js";
+import { assert } from "/lib/util.js";
+
+/**
+ * Upgrade the number of Cores on the home server.
+ *
+ * @param ns The Netscript API.
+ */
+function upgrade_core(ns) {
+    const player = new Player(ns);
+    if (player.money() < ns.singularity.getUpgradeHomeCoresCost()) {
+        return;
+    }
+    const server = new Server(ns, home);
+    ns.tprint("Home Cores before: " + server.cores());
+    const before = intelligence(ns);
+    assert(ns.singularity.upgradeHomeCores());
+    ns.tprint("Home Cores after: " + server.cores());
+    const after = intelligence(ns);
+    const gain = after - before;
+    ns.tprint("Intelligence before: " + before);
+    ns.tprint("Intelligence after: " + after);
+    ns.tprint("Intelligence gain: " + gain);
+    ns.tprint("");
+}
+
+/**
+ * Upgrade the amount of RAM on the home server.
+ *
+ * @param ns The Netscript API.
+ */
+function upgrade_ram(ns) {
+    const player = new Player(ns);
+    if (player.money() < ns.singularity.getUpgradeHomeRamCost()) {
+        return;
+    }
+    const server = new Server(ns, home);
+    ns.tprint("Home RAM before: " + server.ram_max());
+    const before = intelligence(ns);
+    assert(ns.singularity.upgradeHomeRam());
+    ns.tprint("Home RAM after: " + server.ram_max());
+    const after = intelligence(ns);
+    const gain = after - before;
+    ns.tprint("Intelligence before: " + before);
+    ns.tprint("Intelligence after: " + after);
+    ns.tprint("Intelligence gain: " + gain);
+    ns.tprint("");
+}
+
+/**
+ * Determine the amount of Intelligence XP gained from upgrading the Cores and
+ * RAM on the home server.
+ *
+ * @param ns The Netscript API.
+ */
+export async function main(ns) {
+    upgrade_ram(ns);
+    upgrade_core(ns);
+}
