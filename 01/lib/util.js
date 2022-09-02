278f5248ab42ed615eb1f2458aa447654abb1651
Duck McSouls <66396308+quacksouls@users.noreply.github.com>
2022-09-02 20:12:22 +1000

    A library of network functions.

---
+++ 01/lib/util.js
@@ -0,0 +1,45 @@
+/**
+ * Copyright (C) 2022 Duck McSouls
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// Miscellaneous helper functions.
+
+import { home, program } from "/lib/constant.js";
+
+/**
+ * A function for assertion.
+ *
+ * @param cond Assert that this condition is true.
+ * @return Throw an assertion error if the given condition is false.
+ */
+export function assert(cond) {
+    if (!cond) {
+        throw new Error("Assertion failed.");
+    }
+}
+
+/**
+ * Exclude the purchased servers.
+ *
+ * @param ns The Netscript API.
+ * @param server An array of server names.
+ * @return An array of servers, but minus the purchased servers.
+ */
+export function filter_pserv(ns, server) {
+    const pserv = ns.getPurchasedServers();
+    const serv = Array.from(server);
+    return serv.filter(s => !pserv.includes(s));
+}
