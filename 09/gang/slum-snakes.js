6bc8401ae6c84faf248716b77d3ac800b4510d68
Duck McSouls <quacksouls@gmail.com>
2022-09-30 17:28:38 +1000

    Use Slum Snakes to create our gang.

--- 09/gang/slum-snakes.js
+++ 09/gang/slum-snakes.js
@@ -15,10 +15,10 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { bitnode } from "/lib/constant/bn.js";
 import { bool } from "/lib/constant/bool.js";
 import { crimes } from "/lib/constant/crime.js";
 import { faction_req } from "/lib/constant/faction.js";
+import { gang_t } from "/lib/constant/gang.js";
 import { cities } from "/lib/constant/location.js";
 import { home } from "/lib/constant/server.js";
 import { wait_t } from "/lib/constant/time.js";
@@ -26,6 +26,34 @@ import { Player } from "/lib/player.js";
 import { join_faction } from "/lib/singularity/faction.js";
 import { work } from "/lib/singularity/work.js";
 import { assert } from "/lib/util.js";
+
+/**
+ * The karma threshold.  We want to lower our karma to a given amount.  Outside
+ * of BN2, to join a gang we must have karma at -54,000 or lower.  It takes a
+ * very long time to achieve this many negative karma.  A more sensible option
+ * is to have a target karma value and try to lower our karma to that value.
+ * Then move on.  Say we want to lower our karma in 4 batches, each batch is
+ * followed by possibly a soft reset.  In each batch, we would need to achieve
+ * -13,500 karma.
+ *
+ * @param ns The Netscript API.
+ * @return Our current negative karma plus some more negative karma value as
+ *     low as -13,500.  Return 0 if we have enough negative karma to create a
+ *     gang.
+ */
+function karma_threshold(ns) {
+    const player = new Player(ns);
+    const current_karma = Math.floor(player.karma());
+    if (current_karma <= gang_t.KARMA) {
+        return 0;
+    }
+    let target = -13500;
+    const delta = gang_t.KARMA - current_karma;
+    if (Math.abs(delta) < Math.abs(target)) {
+        target = delta;
+    }
+    return Math.floor(current_karma + target);
+}
 
 /**
  * Run the next script(s) in our load chain for criminal gangs.
@@ -38,6 +66,28 @@ function load_chain(ns, faction) {
     const script = "/gang/crime.js";
     const nthread = 1;
     ns.exec(script, home, nthread, faction);
+}
+
+/**
+ * Decrease our karma low enough to allow us to create a gang.  We need -54,000
+ * karma.  Homicide yields -3 karma so we must commit homicide at most 18,000
+ * times.  We lower our karma in batches.  After each batch we might not have
+ * enough negative karma to create a gang.
+ *
+ * @param ns The Netscript API.
+ */
+async function lower_karma(ns) {
+    const threshold = karma_threshold(ns);
+    ns.singularity.goToLocation(cities.generic["slum"]);  // Raise Int XP.
+    ns.singularity.commitCrime(crimes.KILL, bool.FOCUS);
+    const player = new Player(ns);
+    while (Math.floor(player.karma()) > threshold) {
+        if (Math.floor(player.karma()) < gang_t.KARMA) {
+            break;
+        }
+        await ns.sleep(wait_t.DEFAULT);
+    }
+    ns.singularity.stopAction();
 }
 
 /**
@@ -84,10 +134,10 @@ export async function main(ns) {
     // Make the log less verbose.
     ns.disableLog("getServerMoneyAvailable");
     ns.disableLog("sleep");
-    // Should only create a gang within Slum Snakes if we are in BN2.
-    assert(bitnode["Rise of the Underworld"] == ns.getPlayer().bitNodeN);
     // Raise combat stats, ensure we have the required minimum karma, raise our
-    // income.  Then join the Slum Snakes faction.
+    // income.  Then join the Slum Snakes faction.  Lower our karma so we can
+    // create a gang.
+    await lower_karma(ns);
     const fac = "Slum Snakes";
     const player = new Player(ns);
     if (!player.faction().includes(fac)) {
