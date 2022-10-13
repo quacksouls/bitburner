fba5d386e56d6cecf4115311725f88d50d993e30
Duck McSouls <quacksouls@gmail.com>
2022-10-14 09:26:28 +1100

    Use string template.

--- src/cct/trader3.js
+++ src/cct/trader3.js
@@ -57,7 +57,7 @@ export async function main(ns) {
     // Log the result in case of failure.
     if (result.length === 0) {
         const log = "/cct/trader3.txt";
-        const data = "[" + array.join(",") + "]";
+        const data = `[${array.join(",")}]`;
         await log_cct_failure(ns, log, cct, host, data);
         print_error(ns, host, cct);
         return;
