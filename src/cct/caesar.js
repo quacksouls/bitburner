f072fafb9b9bb1d2b85b6bf33a5f5678515357ae
Duck McSouls <quacksouls@gmail.com>
2022-10-14 10:43:25 +1100

    Use type-safe `===`.

--- src/cct/caesar.js
+++ src/cct/caesar.js
@@ -103,7 +103,7 @@ export async function main(ns) {
         returnReward: true,
     });
     // Log the result in case of failure.
-    if (0 == result.length) {
+    if (result.length === 0) {
         const log = "/cct/caesar.txt";
         const data = `[${plaintext}, ${k}]`;
         await log_cct_failure(ns, log, cct, host, data);
