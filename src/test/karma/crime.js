72085650abf9c81cfcb27a1be07247480ce0dac2
Duck McSouls <quacksouls@gmail.com>
2022-10-15 18:32:25 +1100

    Calculate various stat gains

--- src/test/karma/crime.js
+++ src/test/karma/crime.js
@@ -43,6 +43,29 @@ async function commit_crime(ns, crime, howlong) {
 }
 
 /**
+ * The gain in each stat.
+ *
+ * @param ns The Netscript API.
+ * @param start An object following the format of the function player_stat().  Values of various stats before some action.
+ * @param end An object following the format of the function player_stat().  Values of various stats after some action.
+ * @param d The divisor.  Each gained stat is divided by this number to result in an average.  For example, if d
+ *     represents the number of minutes, then the result means the gain per minute.
+ * @return An object following the format of the function player_stat().  Gained values of various stats.
+ */
+function gained_stats(ns, start, end, d) {
+    const denom = Math.floor(d);
+    assert(denom > 0);
+    return {
+        agility: (end.agility - start.agility) / denom,
+        defense: (end.defense - start.defense) / denom,
+        dexterity: (end.dexterity - start.dexterity) / denom,
+        karma: (end.karma - start.karma) / denom,
+        money: (end.money - start.money) / denom,
+        strength: (end.strength - start.strength) / denom,
+    };
+}
+
+/**
  * Whether the given crime is valid.
  *
  * @param crime A string representing the name of a crime.
@@ -62,6 +85,32 @@ function is_valid_crime(crime) {
  */
 function karma(ns) {
     return ns.heart.break();
+}
+
+/**
+ * Various stats of the player.
+ *
+ * @param ns The Netscript API.
+ * @return An object as follows:
+ *
+ *     {
+ *         agility: // The player's agility.
+ *         defense: // The player's defense.
+ *         dexterity: // The player's dexterity.
+ *         karma: // The player's karma.
+ *         money: // The player's money.
+ *         strength: // The player's strength.
+ *     }
+ */
+function player_stat(ns) {
+    return {
+        agility: ns.getPlayer().exp.agility,
+        defense: ns.getPlayer().exp.defense,
+        dexterity: ns.getPlayer().exp.dexterity,
+        karma: karma(ns),
+        money: ns.getPlayer().money,
+        strength: ns.getPlayer().exp.strength,
+    };
 }
 
 /**
@@ -89,18 +138,23 @@ export async function main(ns) {
         usage(ns);
         return;
     }
-    // Commit the given crime.
+    // Commit the given crime and calculate the stat gain per minute.
     const crime = ns.args[0];
+    const start = player_stat(ns);
     const minute = 60;
     const n = 24;
     const howlong = n * wait_t.HOUR;
-    const karma_start = karma(ns);
     await commit_crime(ns, crime, howlong);
-    const karma_end = karma(ns);
-    const karma_earned = karma_end - karma_start;
-    const kpm = karma_earned / (n * minute);
+    const end = player_stat(ns);
+    const divisor = n * minute;
+    const gain = gained_stats(ns, start, end, divisor);
     ns.tprint(`Crime: ${crime}`);
     ns.tprint(`Duration: ${n} hours`);
-    ns.tprint(`Total karma: ${karma_earned}`);
-    ns.tprint(`Karma per minute: ${kpm}`);
+    ns.tprint(`Stat gain per minute.`);
+    ns.tprint(`Agility: ${gain.agility}`);
+    ns.tprint(`Defense: ${gain.defense}`);
+    ns.tprint(`Dexterity: ${gain.dexterity}`);
+    ns.tprint(`Karma: ${gain.karma}`);
+    ns.tprint(`Money: ${gain.money}`);
+    ns.tprint(`Strength: ${gain.strength}`);
 }
