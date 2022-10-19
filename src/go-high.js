d356d88de1f6517d8a73af4e3c3b03f9b3e65eb7
Duck McSouls <quacksouls@gmail.com>
2022-10-19 11:10:56 +1100

    A typ0 fix

--- src/go-high.js
+++ src/go-high.js
@@ -19,8 +19,8 @@ import { home } from "/lib/constant/server.js";
 
 /**
  * This function should be run immediately after the soft reset of installing a
- * bunch of Augmentations.  Our purpose is to gain some money and Hack
- * experience points early on when our stats are low.
+ * bunch of Augmentations.  Our purpose is to gain some money and Hack XP
+ * early on when our stats are low.
  *
  * @param ns The Netscript API.
  */
