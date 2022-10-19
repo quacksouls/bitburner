50808c99244c1e13fcec911b9b7ab8ad9fe4878d
Duck McSouls <quacksouls@gmail.com>
2022-10-19 11:13:04 +1100

    A typ0 fix and proper formatting

--- src/go-high.js
+++ src/go-high.js
@@ -53,9 +53,9 @@ function singularity_scripts(ns) {
 /**
  * NOTE: Assume our home server to have at least 512GB RAM.
  *
- * Restart our source of income and Hack experience points.  This script is
- * useful whenever we have installed a bunch of Augmentations and we want to
- * automatically restart scripts to:
+ * Restart our source of income and Hack XP.  This script is useful whenever we
+ * have installed a bunch of Augmentations and we want to automatically restart
+ * scripts to:
  *
  * (1) Purchase Hacknet nodes and manage our farm of nodes.
  * (2) Buy servers and use each purchased server to hack a target server in the
