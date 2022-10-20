75f56998d90d35c405f2293bf5faa599c42c27ea
Duck McSouls <quacksouls@gmail.com>
2022-10-21 10:43:19 +1100

    Update documentation

--- src/gang/crime.js
+++ src/gang/crime.js
@@ -208,14 +208,12 @@ function enable_turf_war(ns) {
  * (2) weapon
  * (3) armour
  * (4) vehicle
+ * (5) rootkit
  *
- * The Hacker also needs the above equipment, but we should first prioritize
- * equipping them with various rootkits.  The Hacker is the only gang member
- * for whom we purchase rootkits.  No other members should need to use their
- * hacking skills.  If a member already has an Augmentation, weapon, armour,
- * vehicle, or rootkit then we attempt to give them another piece of equipment.
- * Our attempt can succeed or fail, depending on whether we have sufficient
- * funds to purchase equipment.
+ * If a member already has an Augmentation, weapon, armour, vehicle, or rootkit
+ * then we attempt to give them another piece of equipment.  Our attempt can
+ * succeed or fail, depending on whether we have sufficient funds to purchase
+ * equipment.
  *
  * @param ns The Netscript API.
  */
