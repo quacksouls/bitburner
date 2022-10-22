ae8c7af0b7d5e10272e427b5455eb57b9f080daf
Duck McSouls <quacksouls@gmail.com>
2022-10-22 11:24:05 +1100

    Use /***/ to document constants

--- src/lib/constant/bool.js
+++ src/lib/constant/bool.js
@@ -19,66 +19,104 @@
  * Various boolean values.
  */
 export const bool = {
-    // Whether or not a graph is bipartite.
+    /**
+     * Whether or not a graph is bipartite.
+     */
     BIPARTITE: true,
     NOT_BIPARTITE: false,
-    // Whether or not we can run something.
+    /**
+     * Whether or not we can run something.
+     */
     CAN_RUN: true,
     NOT_RUN: false,
-    // Whether or not a graph is directed.
+    /**
+     * Whether or not a graph is directed.
+     */
     DIRECTED: true,
     UNDIRECTED: false,
-    // Enable or disable a feature.
+    /**
+     * Enable or disable a feature.
+     */
     ENABLE: true,
     DISABLE: false,
-    // Whether or not to focus on an action.
+    /**
+     * Whether or not to focus on an action.
+     */
     FOCUS: true,
     NO_FOCUS: false,
-    // Whether or not we have access to something.
+    /**
+     * Whether or not we have access to something.
+     */
     HAS: true,
     NOT: false,
-    // Whether or not it is time for something to occur.
+    /**
+     * Whether or not it is time for something to occur.
+     */
     IS_TIME: true,
     NOT_TIME: false,
-    // Whether or not to join an organization.
+    /**
+     * Whether or not to join an organization.
+     */
     JOIN: true,
     NO_JOIN: false,
-    // Whether or not we can jump.
+    /**
+     * Whether or not we can jump.
+     */
     JUMP: true,
     NO_JUMP: false,
-    // Whether or not a server is low-end.
+    /**
+     * Whether or not a server is low-end.
+     */
     LOWEND: true,
     NOT_LOWEND: false,
-    // Whether or not we can merge two things together.
+    /**
+     * Whether or not we can merge two things together.
+     */
     MERGE: true,
     NO_MERGE: false,
-    // Whether or not we can move from one state to another.
+    /**
+     * Whether or not we can move from one state to another.
+     */
     MOVE: true,
     NOT_MOVE: false,
-    // Whether or not a new state has occurred.
+    /**
+     * Whether or not a new state has occurred.
+     */
     NEW: true,
     NOT_NEW: false,
-    // Whether or not to consider something that is purchased.
+    /**
+     * Whether or not to consider something that is purchased.
+     */
     PURCHASED: true,
     NOT_PURCHASED: false,
-    // Whether or not a state is reachable.  These values are specific to
-    // Array Jumping Game.
+    /**
+     * Whether or not a state is reachable.  These values are specific to
+     * Array Jumping Game.
+     */
     REACHABLE: 1,
     NOT_REACHABLE: 0,
     /**
      * Whether we are self-funding the creation of our corporation.
      */
     SELF_FUND: true,
-    // Whether or not to skip an action.
+    /**
+     * Whether or not to skip an action.
+     */
     SKIP: true,
     NO_SKIP: false,
-    // Whether or not we are successful at something.
+    /**
+     * Whether or not we are successful at something.
+     */
     SUCCESS: true,
     FAILURE: false,
-    // Whether or not something is valid or correct.
+    /**
+     * Whether or not something is valid or correct.
+     */
     VALID: true,
     INVALID: false,
-    // Whether we are (or should be) engaged in territory warfare.
+    /**
+     * Whether we are (or should be) engaged in territory warfare.
+     */
     WAR: true,
     NO_WAR: false,
 };
