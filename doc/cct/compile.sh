3e34dad9728ca42bad1e6f3316000d21bb621747
Duck McSouls <quacksouls@gmail.com>
2022-11-08 11:32:21 +1100

    Doc: CCT: framework for documentation on CCT

---
+++ doc/cct/compile.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Why a shell script to achieve what can be done with a Makefile?  I can't seem
+# to get Make to properly work under Cygwin.  After spending a few hours on
+# the problem, I gave up and wrote this shell script.
+#
+# Why not use a Linux distribution instead of Cygwin?  Some of the programs
+# I use work only on, or better on, Windows.  This forces me to develop
+# using a Windows environment.
+
+DOC=cct
+
+# Compile the document.
+pdflatex "$DOC".tex
+pdflatex "$DOC".tex
+
+# Clean up after ourselves.
+rm *~
+rm .log
+rm *.aux
+rm *.log
+rm *.out
