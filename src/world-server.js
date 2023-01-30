6a1a38635ff06727451798d9ad4ae2fe9bf6a10e
Duck McSouls <quacksouls@gmail.com>
2023-01-30 17:37:19 +1100

    Misc: update script to choose which target server to hack

--- src/world-server.js
+++ src/world-server.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2022 Duck McSouls
+ * Copyright (C) 2022--2023 Duck McSouls
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,17 +16,82 @@
  */
 
 import { bool } from "/lib/constant/bool.js";
+import { server } from "/lib/constant/server.js";
 import { wait_t } from "/lib/constant/time.js";
 import { log } from "/lib/io.js";
+import { darkweb } from "/lib/constant/misc.js";
 import { network } from "/lib/network.js";
 import { Player } from "/lib/player.js";
 import { Server } from "/lib/server.js";
 import {
     assert,
     compromised_servers,
+    has_all_popen,
+    has_program,
     nuke_servers,
-    server_of_max_weight,
 } from "/lib/util.js";
+
+/**
+ * Whether to target the server joesguns.
+ *
+ * @param ns The Netscript API.
+ * @return True if we are to hack joesguns; false otherwise.
+ */
+function choose_joesguns(ns) {
+    assert(has_program(ns, darkweb.program.brutessh.NAME));
+    assert(has_program(ns, darkweb.program.ftpcrack.NAME));
+    return (
+        !has_program(ns, darkweb.program.relaysmtp.NAME)
+        || !has_program(ns, darkweb.program.httpworm.NAME)
+        || !has_program(ns, darkweb.program.sqlinject.NAME)
+    );
+}
+
+/**
+ * Whether to target the server n00dles.
+ *
+ * @param ns The Netscript API.
+ * @return True if we are to hack n00dles; false otherwise.
+ */
+function choose_noodles(ns) {
+    return (
+        !has_program(ns, darkweb.program.brutessh.NAME)
+        || !has_program(ns, darkweb.program.ftpcrack.NAME)
+    );
+}
+
+/**
+ * Whether to target the server phantasy.
+ *
+ * @param ns The Netscript API.
+ * @return True if we are to hack phantasy; false otherwise.
+ */
+function choose_phantasy(ns) {
+    if (!has_all_popen(ns)) {
+        return bool.NOT;
+    }
+    const cutoff = Math.floor(ns.getHackingLevel() / 2);
+    return cutoff >= ns.getServerRequiredHackingLevel(server.PHANTASY);
+}
+
+/**
+ * Choose the target server to hack.
+ *
+ * @param ns The Netscript API.
+ * @return Hostname of the server to target.
+ */
+function choose_target(ns) {
+    if (choose_noodles(ns)) {
+        return server.NOODLES;
+    }
+    if (choose_joesguns(ns)) {
+        return server.JOES;
+    }
+    if (choose_phantasy(ns)) {
+        return server.PHANTASY;
+    }
+    return server.JOES;
+}
 
 /**
  * Deploy our hack script to a nuked server.  Use the server to hack the given
@@ -79,21 +144,20 @@ function shush(ns) {
  * @param ns The Netscript API.
  */
 async function update(ns) {
-    const target = server_of_max_weight(ns, network(ns));
+    const target = choose_target(ns);
     if (!is_new_target(ns, target)) {
         return;
     }
-    log(ns, `Redirect servers to hack common target: ${target}`);
+    log(ns, `Redirect botnet to hack ${target}`);
     const player = new Player(ns);
-    compromised_servers(ns, player.script(), network(ns)).forEach((s) => {
-        ns.killall(s);
-    });
+    const kill_scripts = (host) => ns.killall(host);
+    compromised_servers(ns, player.script(), network(ns)).forEach(kill_scripts);
     nuke_servers(ns, network(ns)).forEach((s) => deploy(ns, s, target));
 }
 
 /**
- * Use each server in the game world to hack a common target.  We exclude
- * purchased servers.
+ * Pool the resources of world servers into a botnet.  Use the botnet to hack a
+ * common target.  We exclude purchased servers.
  *
  * Usage: run world-server.js
  *
