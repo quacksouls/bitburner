7cf3b684e9a2340de0f0d6cf6b0871af889afbe4
Duck McSouls <quacksouls@gmail.com>
2023-03-15 22:50:40 +1100

    Stock: ensure profit to keep is nonnegative

--- src/stock/trade.js
+++ src/stock/trade.js
@@ -297,8 +297,7 @@ async function profit_to_keep(ns, portfolio, profit) {
         keep = Math.floor(keep_mult * profit);
         await ns.sleep(wait_t.MILLISECOND);
     }
-    assert(keep >= 0);
-    return keep;
+    return keep < 0 ? 0 : keep;
 }
 
 /**
