7b39df83e3e547feacf37e293246796859a47b0f
Duck McSouls <quacksouls@gmail.com>
2023-03-24 23:08:02 +1100

    Stock: increase the fraction of money to spend on shares

--- src/lib/constant/wse.js
+++ src/lib/constant/wse.js
@@ -106,7 +106,7 @@ export const wse = {
         /**
          * The same as BUY_MULT, but for "BitNode-8: Ghost of Wall Street".
          */
-        BUY_MULT_BN8: 0.2,
+        BUY_MULT_BN8: 0.3,
         /**
          * Lower the keep fraction by this amount.
          */
