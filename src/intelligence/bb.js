5426ec056943222244a52ac490ff3501ff21961a
Duck McSouls <quacksouls@gmail.com>
2023-06-01 17:14:42 +1000

    Intelligence: manager to farm Intelligence XP

---
+++ src/intelligence/bb.js
@@ -0,0 +1,102 @@
+/**
+ * Copyright (C) 2023 Duck McSouls
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { Bladeburner } from "/quack/lib/bb.js";
+import { bb_t } from "/quack/lib/constant/bb.js";
+import { Sleeve } from "/quack/lib/sleeve/cc.js";
+import { all_sleeves } from "/quack/lib/sleeve/util.js";
+
+/**
+ * Assign a sleeve to take on contracts.
+ *
+ * @param {NS} ns The Netscript API.
+ */
+function contracts(ns) {
+    // Check whether a sleeve is taking on contracts.
+    const sleeve = new Sleeve(ns);
+    if (sleeve.is_performing_contracts()) {
+        if (
+            !sleeve.is_performing_likely_contracts()
+            || sleeve.is_low_on_contracts()
+        ) {
+            ns.sleeve.setToIdle(sleeve.contractor());
+        }
+        return;
+    }
+
+    // See whether we can assign a sleeve to contracts.
+    const bb = new Bladeburner(ns);
+    if (!bb.has_likely_contract()) {
+        return;
+    }
+    sleeve.perform_likely_contracts(bb.likely_contract());
+}
+
+/**
+ * Set the sleeves to perform various initial tasks.
+ *
+ * @param {NS} ns The Netscript API.
+ */
+function init_sleeves(ns) {
+    all_sleeves(ns).forEach((idx) => ns.sleeve.setToIdle(idx));
+    const cc_job = [
+        bb_t.task.FIELD, // 0
+        bb_t.task.DIPLOM, // 1
+        bb_t.task.INFILT, // 2
+    ];
+    const do_task = (task, idx) => ns.sleeve.setToBladeburnerAction(idx, task);
+    cc_job.forEach(do_task);
+}
+
+/**
+ * Suppress various log messages.
+ *
+ * @param {NS} ns The Netscript API.
+ */
+function shush(ns) {
+    ns.disableLog("sleep");
+}
+
+/**
+ * Attempt to upgrade our various skills.
+ *
+ * @param {NS} ns The Netscript API.
+ */
+function upgrade_skills(ns) {
+    const bb = new Bladeburner(ns);
+    bb.upgrade_high_tier_skill();
+    bb.upgrade_low_tier_skill();
+    bb.upgrade_mid_tier_skill();
+}
+
+/**
+ * Use Bladeburner to farm Intelligence XP.
+ *
+ * Usage: run quack/intelligence/bb.js
+ *
+ * @param {NS} ns The Netscript API.
+ */
+export async function main(ns) {
+    shush(ns);
+    init_sleeves(ns);
+
+    for (;;) {
+        contracts(ns);
+        upgrade_skills(ns);
+        await ns.sleep(bb_t.TICK);
+    }
+}
---
+++ src/intelligence/go.js
@@ -0,0 +1,137 @@
+/**
+ * Copyright (C) 2023 Duck McSouls
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { bb_t } from "/quack/lib/constant/bb.js";
+import { bool } from "/quack/lib/constant/bool.js";
+import { cc_t } from "/quack/lib/constant/sleeve.js";
+import { wait_t } from "/quack/lib/constant/time.js";
+import { error, log } from "/quack/lib/io.js";
+import { Player } from "/quack/lib/player.js";
+import { has_all_sleeves } from "/quack/lib/sleeve/util.js";
+import {
+    has_ai_api,
+    has_bladeburner_api,
+    has_singularity_api,
+    has_sleeve_api,
+} from "/quack/lib/source.js";
+import { exec } from "/quack/lib/util.js";
+
+/**
+ * Whether we have the required APIs:
+ *
+ * (1) Singularity
+ * (2) Sleeves
+ * (3) Bladeburner
+ * (4) Intelligence
+ *
+ * @param {NS} ns The Netscript API.
+ * @returns {boolean} True if we have the necessary APIs; false otherwise.
+ */
+function has_api_access(ns) {
+    const sanity = [
+        { func: has_ai_api, msg: "No access to Artificial Intelligence API" },
+        { func: has_singularity_api, msg: "No access to Singularity API" },
+        { func: has_bladeburner_api, msg: "No access to Bladeburner API" },
+        { func: has_sleeve_api, msg: "No access to Sleeve API" },
+        {
+            func: has_all_sleeves,
+            msg: `Require ${cc_t.MAX_SLEEVE} sleeves for Bladeburner`,
+        },
+    ];
+    for (const obj of sanity) {
+        if (!obj.func(ns)) {
+            error(ns, obj.msg);
+            return bool.NOT;
+        }
+    }
+
+    return bool.HAS;
+}
+
+/**
+ * Join the Bladeburner division.
+ *
+ * @param {NS} ns The Netscript API.
+ */
+async function join(ns) {
+    while (!ns.bladeburner.joinBladeburnerDivision()) {
+        await ns.sleep(wait_t.SECOND);
+    }
+    log(ns, "Joined Bladeburner division");
+}
+
+/**
+ * Wait until we have the required combat stats for Bladeburner.  We require a
+ * minimum of 100 in each of Strength, Defense, Dexterity, and Agility to join
+ * the Bladeburner division of the CIA.
+ *
+ * @param {NS} ns The Netscript API.
+ */
+async function required_stats(ns) {
+    const player = new Player(ns);
+    const stat = () => [
+        player.agility(),
+        player.defense(),
+        player.dexterity(),
+        player.strength(),
+    ];
+    const below_minimum = (s) => s < bb_t.stat.combat.MIN;
+    while (stat().some(below_minimum)) {
+        await ns.sleep(wait_t.DEFAULT);
+    }
+}
+
+/**
+ * Suppress various log messages.
+ *
+ * @param {NS} ns The Netscript API.
+ */
+function shush(ns) {
+    ns.disableLog("sleep");
+}
+
+/**
+ * Manager for farming Intelligence XP via the following techniques:
+ *
+ * (1) The main avatar commits larceny.
+ * (2) Purchase a batch of programs, then delete them.  Rinse and repeat.
+ * (3) Bladeburner and sleeves.  Sleeves commit various Bladeburner actions and
+ *     contracts.  Main avatar performs Bladeburner operations.
+ *
+ * Usage: run quack/intelligence/go.js
+ *
+ * @param {NS} ns The Netscript API.
+ */
+export async function main(ns) {
+    shush(ns);
+
+    if (!has_api_access(ns)) {
+        return;
+    }
+
+    exec(ns, "/quack/intelligence/exe.js");
+    exec(ns, "/quack/intelligence/larceny.js");
+
+    if (ns.bladeburner.inBladeburner()) {
+        log(ns, "Already in Bladeburner division");
+        exec(ns, "/quack/intelligence/bb.js");
+    } else {
+        await required_stats(ns);
+        await join(ns);
+        exec(ns, "/quack/intelligence/bb.js");
+    }
+}
